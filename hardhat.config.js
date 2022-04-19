37eadd2a2485932a6d2f73e466fccb50884d8fff
imroshin <imroshin@gmail.com>
2022-04-19 22:59:08 +0530

    hardhat config update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+const dotenv = require("dotenv");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+dotenv.config();
+
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -10,12 +13,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby:{
+      url: process.env.REACT_APP_RINKEBY_RPC_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY]
+    },
+  },
+  etherscan:{
+    apiKey: process.env.REACT_APP_ETHERSCAN_KEY
+  }
 };
