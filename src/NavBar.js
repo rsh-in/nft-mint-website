88ec39fd7a5f6e60ddf232ee1baf60f83c2d0000
imroshin <imroshin@gmail.com>
2022-04-20 17:17:10 +0530

    Nav Bar

---
+++ src/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const NavBar = ({ accounts, setAccounts }) => {
+    const isConnected = Boolean(accounts[0]);
+
+    async function connectAccount() {
+        if (window.ethereum) {
+            const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            setAccounts(accounts);
+        }
+    }
+
+    return (
+        <div>
+            {/* Left Side - Social Media Icons */}
+            <div>Facebook</div>
+            <div>Twitter</div>
+            <div>Email</div>
+
+            {/*Right Side - Sections and Connect */}
+            <div>About</div>
+            <div>Mint</div>
+            <div>Team</div>
+
+            {/* Connect */}
+            
+        </div>
+    )
+}
