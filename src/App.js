27a1aa446895d098e79a60ad3d4f71da7a7b415d
imroshin <imroshin@gmail.com>
2022-04-24 23:41:58 +0530

    added new divs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import MainMint from  './MainMint';
+import NavBar from './NavBar';
 
 function App() {
+  const [accounts, setAccounts] = useState([]) 
   return (
+    <div className='overlay'>
+
     <div className="App">
-      
+
+      <NavBar accounts={accounts} setAccounts={setAccounts} />
+      <MainMint accounts={accounts} setAccounts={setAccounts}/>
+    </div>
+    <div className='moving-background'></div>
     </div>
   );
 }
