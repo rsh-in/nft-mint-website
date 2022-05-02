2339d0dc50c0ba2372914a94e5f873792bd437d5
imroshin <imroshin@gmail.com>
2022-05-02 20:12:37 +0530

    updated token uri

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,11 +3,12 @@ const { ethers } = require("hardhat");
 async function main(){
   const Arcane = await ethers.getContractFactory("Arcane") //reference to our contract
   const arcane = await Arcane.deploy()
-  await arcane.setBaseTokenURI("https://ipfs.io/ipfs/QmYHNiQr5cuzQgisuV1cVzjNhaoUL4B8Hx6XQg7tzGBfRF") //passing in the URI of the metadata json
 
   try{
     await arcane.deployed()
-    console.log(`Contract successfully deployed to ${arcane.address}`)
+    console.log(`Contract successfully deployed to ${arcane.address} arcaneee`)
+    await arcane.setBaseTokenUri("https://ipfs.io/ipfs/QmZU4L6ud69SdDknTw7WFAWTGGy98fZFoaG217pgoZbh4u") //passing in the URI of the metadata json
+    console.log(`Base token URI set`)
     // mintNFT()
   } catch(err){
     console.log(`Error: ${err.message}`)
