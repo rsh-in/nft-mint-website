4c54b94cc09764c9f02f1577f17ef914d8dcec94
imroshin <imroshin@gmail.com>
2022-05-02 20:13:24 +0530

    added polygon network

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,14 +18,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.11",
   networks: {
     rinkeby:{
       url: process.env.REACT_APP_RINKEBY_RPC_URL,
       accounts: [process.env.REACT_APP_PRIVATE_KEY]
     },
+    mumbai: {
+      url:process.env.REACT_APP_MUMBAI_RPC_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY]
+    }
   },
   etherscan:{
-    apiKey: process.env.REACT_APP_ETHERSCAN_KEY
+    apiKey: process.env.REACT_APP_POLYGONSCAN_KEY
   }
 };
